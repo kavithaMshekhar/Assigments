403172c40c17dfdde027af4b23f7e59ea38a2483
kavithaMshekhar <kavi.201986@gmail.com>
2026-04-28 19:52:25 +0530

    Assigments

--- Assigments/assigment10.ts
+++ Assigments/assigment10.ts
@@ -1,4 +1,4 @@
-// Best Time to Buy and Sell Stock
+// Best Time to Buy and Sell Stock (price always decreasing no profit is possible)
 function maxProfit(prices: number[]): number {
   let minPrice :number = Infinity;
   let maxProfit :number= 0;
@@ -19,4 +19,24 @@ function maxProfit(prices: number[]): number {
 }
 // function call
 console.log(maxProfit([7, 1, 5, 3, 6 ,4]));   // out put is 5
+ /* Day 1 (price = 7)
+minPrice = 7  //(first value)
+profit = 7 - 7 = 0
+maxProfit = 0 */
+/*Day 2 (price = 1)
+minPrice = 1  //(better buying price!)
+profit = 1 - 1 = 0
+maxProfit = 0*/
+/*Day 3 (price = 5)
+profit = 5 - 1 = 4
+maxProfit = 4*/
+/*Day 4 (price = 3)
+profit = 3 - 1 = 2
+maxProfit = 4 */
+/*Day 5 (price = 6)
+profit = 6 - 1 = 5
+maxProfit = 5*/ //(better selling day)
+/*profit = 4 - 1 = 3
+maxProfit = 5*/
+
 console.log(maxProfit([7, 6, 4, 3, 1]));       // output is 0
